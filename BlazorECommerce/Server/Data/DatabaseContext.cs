ec6670d8ec225abdbba1b82bba63e2ee32d53007
Michael Bond <michael@bondcodes.com>
2022-10-27 15:59:30 -0400

    Seed data

--- BlazorECommerce/Server/Data/DatabaseContext.cs
+++ BlazorECommerce/Server/Data/DatabaseContext.cs
@@ -4,5 +4,41 @@ public class DatabaseContext : DbContext
 {
     protected DatabaseContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Product>().HasData(new List<Product>()
+        {
+            new Product
+            {
+                Id = 1,
+                Title = "Product 1 Title",
+                Description = "Product 1 Description",
+                ImageUrl =
+                    "https://img.freepik.com/free-psd/cosmetic-product-packaging-mockup_1150-40284.jpg?w=2000",
+                Price = 9.99M
+            },
+            new Product
+            {
+                Id = 2,
+                Title = "Product 2 Title",
+                Description = "Product 2 Description",
+                ImageUrl =
+                    "https://img.freepik.com/free-psd/cosmetic-product-packaging-mockup_1150-40282.jpg?w=2000",
+                Price = 9.99M
+            },
+            new Product
+            {
+                Id = 3,
+                Title = "Product 3 Title",
+                Description = "Product 3 Description",
+                ImageUrl =
+                    "https://img.freepik.com/free-photo/pedestal-display-blank-podium-product_1048-16154.jpg?w=996",
+                Price = 9.99M
+            },
+        });
+    }
+
     public virtual DbSet<Product> Products { get; set; } = default!;
 }
--- BlazorECommerce/Server/Endpoints/ProductApi.cs
+++ BlazorECommerce/Server/Endpoints/ProductApi.cs
@@ -2,36 +2,7 @@
 
 public static class ProductApi
 {
-    private static readonly List<Product> Products = new()
-    {
-        new Product
-        {
-            Id = 1,
-            Title = "Product 1 Title",
-            Description = "Product 1 Description",
-            ImageUrl =
-                "https://img.freepik.com/free-psd/cosmetic-product-packaging-mockup_1150-40284.jpg?w=2000",
-            Price = 9.99M
-        },
-        new Product
-        {
-            Id = 2,
-            Title = "Product 2 Title",
-            Description = "Product 2 Description",
-            ImageUrl =
-                "https://img.freepik.com/free-psd/cosmetic-product-packaging-mockup_1150-40282.jpg?w=2000",
-            Price = 9.99M
-        },
-        new Product
-        {
-            Id = 3,
-            Title = "Product 3 Title",
-            Description = "Product 3 Description",
-            ImageUrl =
-                "https://img.freepik.com/free-photo/pedestal-display-blank-podium-product_1048-16154.jpg?w=996",
-            Price = 9.99M
-        },
-    };
+    private static readonly List<Product> Products = new();
 
     public static IEndpointRouteBuilder MapProductApi(this IEndpointRouteBuilder apiGroup)
     {
