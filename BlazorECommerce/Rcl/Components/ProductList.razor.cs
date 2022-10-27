a76f174bcb85f41cce817eecff7926762eda15a3
Michael Bond <michael@bondcodes.com>
2022-10-27 17:01:19 -0400

    Store products in service

--- BlazorECommerce/Rcl/Components/ProductList.razor.cs
+++ BlazorECommerce/Rcl/Components/ProductList.razor.cs
@@ -2,11 +2,5 @@
 
 public partial class ProductList
 {
-    private static List<Product> _products = new();
-
-    protected override async Task OnInitializedAsync()
-    {
-        await ProductService.GetProductsAsync();
-        _products = ProductService.Products;
-    }
+    protected override async Task OnInitializedAsync() => await ProductService.GetProductsAsync();
 }
