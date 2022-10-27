d53b09f0bb60895ed1c98979fa5e185e9f625586
Michael Bond <michael@bondcodes.com>
2022-10-27 15:53:13 -0400

    Move using

--- BlazorECommerce/Shared/GlobalUsings.cs
+++ BlazorECommerce/Shared/GlobalUsings.cs
@@ -1,1 +1,2 @@
-﻿global using System.Runtime.Serialization;
+﻿global using System.ComponentModel.DataAnnotations.Schema;
+global using System.Runtime.Serialization;
--- BlazorECommerce/Shared/Models/Product.cs
+++ BlazorECommerce/Shared/Models/Product.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
-namespace BlazorECommerce.Shared.Models;
+﻿namespace BlazorECommerce.Shared.Models;
 
 public class Product
 {
