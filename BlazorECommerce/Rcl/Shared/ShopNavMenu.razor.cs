fa3a575bacfecb4f9ae891aec60ce969145d11a1
Michael Bond <codemonkey8510@icloud.com>
2022-10-28 18:07:48 -0400

    Layout changes

---
+++ BlazorECommerce/Rcl/Shared/ShopNavMenu.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorECommerce.Rcl.Shared;
+
+public partial class ShopNavMenu
+{
+    private bool _collapseNavMenu = true;
+
+    private string? NavMenuCssClass => _collapseNavMenu ? "collapse" : null;
+
+    private void ToggleNavMenu() => _collapseNavMenu = !_collapseNavMenu;
+
+    protected override async Task OnInitializedAsync() => await CategoryService.GetCategoriesAsync();
+}
