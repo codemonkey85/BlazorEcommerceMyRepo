b77930b36ee90ac13744d69e9997b5d5da27aabc
Michael Bond <michael@bondcodes.com>
2022-10-30 19:09:04 -0400

    Rework getting cart products

--- BlazorECommerce/Server/Services/CartServices/CartService.cs
+++ BlazorECommerce/Server/Services/CartServices/CartService.cs
@@ -2,38 +2,28 @@
 
 public record CartService(DatabaseContext DatabaseContext) : ICartService
 {
-    public async Task<ServiceResponse<List<CartProductResponse>>> GetCartProductsAsync(List<CartItem> cartItems)
-    {
-        var result = new ServiceResponse<List<CartProductResponse>>
+    public Task<ServiceResponse<List<CartProductResponse>>> GetCartProductsAsync(List<CartItem> cartItems) =>
+        Task.FromResult(new ServiceResponse<List<CartProductResponse>>
         {
-            Data = new List<CartProductResponse>()
-        };
-
-        foreach (var cartItem in cartItems)
-        {
-            var cartProduct = await (
-                from products in DatabaseContext.Products
-                join productVariants in DatabaseContext.ProductVariants.Include(v => v.ProductType) on new
-                { ProductId = products.Id, cartItem.ProductTypeId } equals new
-                { productVariants.ProductId, productVariants.ProductTypeId }
-                where products.Id == cartItem.ProductId && productVariants.ProductTypeId == cartItem.ProductTypeId
-                select new CartProductResponse
-                {
-                    ProductId = products.Id,
-                    Title = products.Title,
-                    ImageUrl = products.ImageUrl,
-                    Price = productVariants.Price,
-                    ProductType = productVariants.ProductType.Name,
-                    ProductTypeId = productVariants.ProductTypeId,
-                    Quantity = cartItem.Quantity,
-                }).FirstOrDefaultAsync();
-
-            if (cartProduct is not null)
-            {
-                result.Data.Add(cartProduct);
-            }
-        }
-
-        return result;
-    }
+            Data = new List<CartProductResponse>(cartItems.Select(cartItem => (
+                    from products in DatabaseContext.Products
+                    join productVariants in DatabaseContext.ProductVariants.Include(v => v.ProductType) on
+                        new
+                        { ProductId = products.Id, cartItem.ProductTypeId }
+                        equals new
+                        { productVariants.ProductId, productVariants.ProductTypeId }
+                    where products.Id == cartItem.ProductId && productVariants.ProductTypeId == cartItem.ProductTypeId
+                    select new CartProductResponse
+                    {
+                        ProductId = products.Id,
+                        Title = products.Title,
+                        ImageUrl = products.ImageUrl,
+                        Price = productVariants.Price,
+                        ProductType = productVariants.ProductType.Name,
+                        ProductTypeId = productVariants.ProductTypeId,
+                        Quantity = cartItem.Quantity,
+                    }
+                    ).First()
+            ))
+        });
 }
