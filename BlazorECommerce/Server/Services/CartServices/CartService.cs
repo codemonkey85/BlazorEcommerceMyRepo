91b7c9a06d40f188f64cfa17bcdbb450263453f5
Michael Bond <codemonkey8510@icloud.com>
2022-10-31 09:35:46 -0400

    Order the cart items

--- BlazorECommerce/Server/Services/CartServices/CartService.cs
+++ BlazorECommerce/Server/Services/CartServices/CartService.cs
@@ -6,24 +6,26 @@ public record CartService(DatabaseContext DatabaseContext) : ICartService
         Task.FromResult(new ServiceResponse<List<CartProductResponse>>
         {
             Data = new List<CartProductResponse>(cartItems.Select(cartItem => (
-                    from products in DatabaseContext.Products
-                    join productVariants in DatabaseContext.ProductVariants.Include(v => v.ProductType) on
-                        new
-                        { ProductId = products.Id, cartItem.ProductTypeId }
-                        equals new
-                        { productVariants.ProductId, productVariants.ProductTypeId }
-                    where products.Id == cartItem.ProductId && productVariants.ProductTypeId == cartItem.ProductTypeId
-                    select new CartProductResponse
-                    {
-                        ProductId = products.Id,
-                        Title = products.Title,
-                        ImageUrl = products.ImageUrl,
-                        Price = productVariants.Price,
-                        ProductType = productVariants.ProductType.Name,
-                        ProductTypeId = productVariants.ProductTypeId,
-                        Quantity = cartItem.Quantity,
-                    }
+                        from products in DatabaseContext.Products
+                        join productVariants in DatabaseContext.ProductVariants.Include(v => v.ProductType) on
+                            new { ProductId = products.Id, cartItem.ProductTypeId }
+                            equals new { productVariants.ProductId, productVariants.ProductTypeId }
+                        where products.Id == cartItem.ProductId &&
+                              productVariants.ProductTypeId == cartItem.ProductTypeId
+                        select new CartProductResponse
+                        {
+                            ProductId = products.Id,
+                            Title = products.Title,
+                            ImageUrl = products.ImageUrl,
+                            Price = productVariants.Price,
+                            ProductType = productVariants.ProductType.Name,
+                            ProductTypeId = productVariants.ProductTypeId,
+                            Quantity = cartItem.Quantity,
+                        }
                     ).First()
-            ))
+                )
+                .OrderBy(c => c.ProductId)
+                .ThenBy(c => c.ProductTypeId)
+            )
         });
 }
