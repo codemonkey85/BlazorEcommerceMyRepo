78a6ef8b7d71264846a8da3cc60e9f24ffbedf18
Michael Bond <codemonkey8510@icloud.com>
2022-11-06 15:25:32 -0500

    Simplify client services registration

--- BlazorECommerce/Client/Program.cs
+++ BlazorECommerce/Client/Program.cs
@@ -2,9 +2,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-var services = builder.Services;
-
-services
+builder.Services
     .AddBlazoredLocalStorage()
     .AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
     .AddScoped<IAuthService, AuthService>()
