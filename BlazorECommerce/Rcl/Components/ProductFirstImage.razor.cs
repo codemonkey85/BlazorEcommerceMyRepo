970ad1d9903c89059b23c361ef902b45efcb29c5
Michael Bond <codemonkey8510@icloud.com>
2022-12-16 18:34:19 -0500

    Fix cart image sorta

--- BlazorECommerce/Rcl/Components/ProductFirstImage.razor.cs
+++ BlazorECommerce/Rcl/Components/ProductFirstImage.razor.cs
@@ -2,7 +2,18 @@ namespace BlazorECommerce.Rcl.Components;
 
 public partial class ProductFirstImage
 {
-    [Parameter, EditorRequired] public Product? Product { get; set; }
+    [Parameter] public Product? Product { get; set; }
+
+    [Parameter] public CartProductResponse? CartProductResponse { get; set; }
 
     [Parameter] public string CssClass { get; set; } = string.Empty;
+
+    protected override async Task OnParametersSetAsync()
+    {
+        if (Product is null && CartProductResponse is not null)
+        {
+            var response = await ProductService.GetProductAsync(CartProductResponse.ProductId);
+            Product = response.Data;
+        }
+    }
 }
